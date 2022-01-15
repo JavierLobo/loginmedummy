1fe9b07927692a4e29e4c941280bbbd023ccdbf5
Javier Lobo <franciscojavier.lobo@gmail.com>
2022-01-15 16:07:31 +0100

    (DEV) Initial Commit

---
+++ zuulserver/src/test/java/com/javierlobo/loginmedummy/zuulserver/ZuulserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javierlobo.loginmedummy.zuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
