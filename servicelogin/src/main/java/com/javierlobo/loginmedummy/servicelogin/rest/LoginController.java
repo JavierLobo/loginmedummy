b1a03e68bced08df90906cda1b111281254d6d5f
Javier Lobo <franciscojavier.lobo@gmail.com>
2022-02-05 22:38:12 +0100

    (DEV) Add basic configuration

--- servicelogin/src/main/java/com/javierlobo/loginmedummy/servicelogin/rest/LoginController.java
+++ servicelogin/src/main/java/com/javierlobo/loginmedummy/servicelogin/rest/LoginController.java
@@ -1,5 +1,6 @@
 package com.javierlobo.loginmedummy.servicelogin.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,9 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class LoginController {
 	
+	@Value("${configuracion.texto}")
+	//@Value("Hola Mundo")
+	private String message; // =  "Hello World!!!";
+	
+	@Value("${eureka.instance.instance-id}") 
+	private String instanceId;
+
     @GetMapping("/whois")
     public @ResponseBody String hello() {
-        return "Hello World!!!";
+    	System.out.println(String.valueOf(instanceId).concat(" :--: ").concat(message));
+        return message;
     }
 
 }
