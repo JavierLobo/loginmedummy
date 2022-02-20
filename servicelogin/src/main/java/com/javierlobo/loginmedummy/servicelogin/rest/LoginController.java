a3072e271430a2f61f8aa1fe44d1feb6f21e80bd
Javier Lobo <franciscojavier.lobo@gmail.com>
2022-02-20 21:59:19 +0100

    Revert "(DEV) Service identification"
    This reverts commit 61bfbf80ad2a4395ca38fcc49d6933f418a9b48e.

--- servicelogin/src/main/java/com/javierlobo/loginmedummy/servicelogin/rest/LoginController.java
+++ servicelogin/src/main/java/com/javierlobo/loginmedummy/servicelogin/rest/LoginController.java
@@ -10,12 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class LoginController {
 	
+	@Value("${configuracion.texto}")
+	//@Value("Hola Mundo")
+	private String message; // =  "Hello World!!!";
+	
 	@Value("${eureka.instance.instance-id}") 
 	private String instanceId;
 
-    @GetMapping("/whoim")
+    @GetMapping("/whois")
     public @ResponseBody String hello() {
-        return instanceId;
+    	System.out.println(String.valueOf(instanceId).concat(" :--: ").concat(message));
+        return message;
     }
 
 }
