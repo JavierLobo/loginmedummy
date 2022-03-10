5fa96cb5599b80fa94da539a914fb3fabef5ddb7
Javier Lobo <franciscojavier.lobo@gmail.com>
2022-03-11 00:05:01 +0100

    (DEV) Add Zuul Server

---
+++ zuulserver/src/main/java/com/javierlobo/loginmedummy/zuulserver/CustomFilter.java
@@ -0,0 +1,58 @@
+package com.javierlobo.loginmedummy.ZuulServer;
+
+import java.util.Enumeration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+public class CustomFilter extends ZuulFilter {
+
+  private static Logger log = LoggerFactory.getLogger(CustomFilter.class);
+  
+  private String filterType; 
+  
+  public CustomFilter(String filterType) {
+	  this.filterType = filterType;
+  }
+  
+  @Override
+  public String filterType() {
+    return filterType;
+  }
+
+  @Override
+  public int filterOrder() {
+	  return FilterConstants.SEND_RESPONSE_FILTER_ORDER;
+  }
+
+  @Override
+  public boolean shouldFilter() {
+    return true;
+  }
+
+  @Override
+  public Object run() {
+    RequestContext ctx = RequestContext.getCurrentContext();
+    StringBuffer strLog = new StringBuffer();
+    
+    strLog.append("\n------ NUEVA PETICION ------\n");                     
+    strLog.append(String.format("Server: %s Metodo: %s Path: %s \n",
+    		ctx.getRequest().getServerName() ,ctx.getRequest().getMethod(), ctx.getRequest().getRequestURI()));
+    
+    Enumeration<String> enume= ctx.getRequest().getHeaderNames();
+    
+    String header;    
+    while (enume.hasMoreElements()) {
+        header = enume.nextElement();
+        strLog.append(String.format("Headers: %s = %s \n", header, ctx.getRequest().getHeader(header)));                 
+    };
+    
+    log.info(strLog.toString());
+    return null;
+  }
+
+}
--- zuulserver/src/main/java/com/javierlobo/loginmedummy/zuulserver/ZuulserverApplication.java
+++ zuulserver/src/main/java/com/javierlobo/loginmedummy/zuulserver/ZuulserverApplication.java
@@ -3,9 +3,13 @@ package com.javierlobo.loginmedummy.ZuulServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class ZuulServerApplication extends SpringBootServletInitializer {
 
@@ -13,4 +17,29 @@ public class ZuulServerApplication extends SpringBootServletInitializer {
 		SpringApplication.run(ZuulServerApplication.class, args);
 	}
 
+//	@Bean
+//	public CustomFilter preFilter() {
+//		return new CustomFilter(FilterConstants.PRE_TYPE);
+//	}
+//	
+//	@Bean
+//	public CustomFilter postFilter() {
+//		return new CustomFilter(FilterConstants.POST_TYPE);
+//	}
+//	
+//	@Bean
+//	public CustomFilter errorFilter() {
+//		return new CustomFilter(FilterConstants.ERROR_TYPE);
+//	}
 }
+
+/*
+ * URL's:
+ *   - http://localhost:8005/api/whois
+ *   - http://localhost:8090/gateway/api/whois
+ *   
+ * 
+ * Notas: 
+ * https://www.google.com/search?q=spring+zuul+server&sxsrf=APq-WBsZzbJ5-C3LeV-iNcId-bLsYQfrKQ%3A1646773851572&ei=W8YnYum_Iv-W9u8P49-FiA0&oq=spring+zuul&gs_lcp=Cgdnd3Mtd2l6EAMYADIECCMQJzIFCAAQywEyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBMgUIABDLAToHCCMQsAMQJzoHCAAQRxCwAzoHCAAQsAMQQzoKCAAQ5AIQsAMYADoSCC4QxwEQowIQyAMQsAMQQxgBOhUILhDHARDRAxDUAhDIAxCwAxBDGAE6DwguENQCEMgDELADEEMYAToSCC4QxwEQ0QMQyAMQsAMQQxgBOgoIABCxAxCDARBDOgcIABCxAxBDOgQIABBDOggIABCABBCxAzoLCAAQgAQQsQMQgwE6BQgAEIAEOgQIABAKSgQIQRgASgQIRhgBUIgFWM0TYP0eaAFwAXgAgAF0iAHRA5IBAzQuMZgBAKABAcgBE8ABAdoBBggAEAEYCdoBBggBEAEYCA&sclient=gws-wiz
+ * http://www.profesor-p.com/2019/03/16/zuul-para-redirigir-peticiones-rest-en-spring-boot/
+ */
